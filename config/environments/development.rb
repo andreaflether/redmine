f0616e9ecddab6324310edbec4a356711f76e716
Andréa <andreaflether@gmail.com>
2022-09-09 22:00:43 +0000

    Gitpod config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "3000-andreaflether-redmine-mfgm17vrbpn.ws-us64.gitpod.io"
 end
